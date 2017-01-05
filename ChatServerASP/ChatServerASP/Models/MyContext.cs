278dad7af588211a620969951edbe08953bab532
WickedSick0 <nycdavid@sssvt.cz>
2017-01-05 23:18:28 +0100

    fgvh

--- ChatServerASP/ChatServerASP/Models/MyContext.cs
+++ ChatServerASP/ChatServerASP/Models/MyContext.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
 using System.Linq;
 using System.Web;
 
@@ -9,6 +10,8 @@ namespace ChatServerASP.Models
     public class MyContext : DbContext
     {
         public DbSet<User> Users { get; set; }
+
+
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
