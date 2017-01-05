cb816ddbe2da82ab56334aeb635c52a4d83c8359
WickedSick0 <nycdavid@sssvt.cz>
2017-01-05 22:18:39 +0100

    NO VOLE

---
+++ ChatServerASP/ChatServerASP/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ChatServerASP.MvcApplication" Language="C#" %>
---
+++ ChatServerASP/ChatServerASP/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ChatServerASP.Startup))]
+namespace ChatServerASP
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
