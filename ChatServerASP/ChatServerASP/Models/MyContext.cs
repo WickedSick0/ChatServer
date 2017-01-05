8f6294f8f4c58a1bd12c688393f70e54f16977cd
WickedSick0 <nycdavid@sssvt.cz>
2017-01-05 23:16:40 +0100

    context

---
+++ ChatServerASP/ChatServerASP/Models/MyContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ChatServerASP.Models
+{
+    public class MyContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+    }
+}
