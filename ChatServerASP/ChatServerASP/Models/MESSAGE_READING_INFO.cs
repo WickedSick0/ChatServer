9fd2b28b8f6993d9a1992fce3a533defa3c1239e
WickedSick0 <nycdavid@sssvt.cz>
2017-01-10 20:23:41 +0100

    message_reading_info

---
+++ ChatServerASP/ChatServerASP/Models/MESSAGE_READING_INFO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ChatServerASP.Models
+{
+    public class MESSAGE_READING_INFO
+    {
+        public int ID_Chatroom { get; set; }
+
+        public int ID_Message { get; set; }
+
+        public int ID_Read_By_User { get; set; }
+    }
+}
