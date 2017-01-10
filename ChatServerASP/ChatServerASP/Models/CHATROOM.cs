a2264be584b07f0d71081977d60315d2a46cbab2
WickedSick0 <nycdavid@sssvt.cz>
2017-01-10 20:17:40 +0100

    chatroom

---
+++ ChatServerASP/ChatServerASP/Models/CHATROOM.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ChatServerASP.Models
+{
+    public class CHATROOM
+    {
+        public int ID { get; set; }
+
+        public string Chatroom_Name { get; set; }
+    }
+}
