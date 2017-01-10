6f56c1d9d61a672ee5e46d3fd1d2ccbe4e3f211b
WickedSick0 <nycdavid@sssvt.cz>
2017-01-10 20:25:17 +0100

    user_friends

---
+++ ChatServerASP/ChatServerASP/Models/USER_FRIENDS.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ChatServerASP.Models
+{
+    public class USER_FRIENDS
+    {
+        public int ID { get; set; }
+
+        public int ID_Friend { get; set; }
+
+        public int ID_Friendlist_Owner { get; set; }
+    }
+}
