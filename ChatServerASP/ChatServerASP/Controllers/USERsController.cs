e6890c7fec46abf31badfe9b18c498288f33e7a1
Stitek <stipekjan@sssvt.cz>
2017-01-10 22:15:49 +0100

    USERsController

---
+++ ChatServerASP/ChatServerASP/Controllers/USERsController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ChatServerASP.Controllers
+{
+    public class USERsController : ApiController
+    {
+        // GET: api/USERs
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/USERs/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/USERs
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/USERs/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/USERs/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
