84b7f4c8f2a095e11634d321405eb8a4166acfff
WickedSick0 <nycdavid@sssvt.cz>
2017-01-10 20:21:47 +0100

    message

---
+++ ChatServerASP/ChatServerASP/Models/MESSAGE.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ChatServerASP.Models
+{
+    public class MESSAGE
+    {
+        public int ID { get; set; }
+
+        public int ID_Chatroom { get; set; }
+
+        public int ID_User_Post { get; set; }
+
+        public string Message_text { get; set; }
+
+    }
+}
