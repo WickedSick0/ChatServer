05026342650837e258dcd9a07bf6db6875cb3c05
Stitek <stipekjan@sssvt.cz>
2017-01-10 20:55:08 +0100

    Chatroom_membersRepository

---
+++ ChatServerASP/ChatServerASP/Models/Chatroom_membersRepository.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ChatServerASP.Models
+{
+    public class Chatroom_membersRepository
+    {
+        private MyContext _context = new MyContext();
+
+        public List<CHATROOM_MEMBERS> FindAll()
+        {
+            return this._context.Chatroom_members.ToList<CHATROOM_MEMBERS>();
+        }
+
+        public CHATROOM_MEMBERS FindById(int id)
+        {
+            return this._context.Chatroom_members.Find(id);
+        }
+
+        public void InsertChatroom_members(CHATROOM_MEMBERS cm)
+        {
+            this._context.Chatroom_members.Add(cm);
+            this._context.SaveChanges();
+        }
+
+        /*
+        public void UpdateChatroom_members(CHATROOM_MEMBERS cm)
+        {
+            CHATROOM_MEMBERS cmtemp = this.FindById(cm.ID);
+
+            cmtemp.ID_Chatroom = cm.ID_Chatroom;
+            cmtemp.ID_User = cm.ID_User;
+
+            this._context.SaveChanges();
+        }
+        */
+        public void DeleteChatroom_members(int id)
+        {
+            CHATROOM_MEMBERS cm = this.FindById(id);
+            this._context.Chatroom_members.Remove(cm);
+            this._context.SaveChanges();
+        }
+    }
+}
--- ChatServerASP/ChatServerASP/Models/MyContext.cs
+++ ChatServerASP/ChatServerASP/Models/MyContext.cs
@@ -9,11 +9,15 @@ namespace ChatServerASP.Models
 {
     public class MyContext : DbContext
     {
+        public DbSet<CHATROOM> Chatrooms { get; set; }
+
+        public DbSet<CHATROOM_MEMBERS> Chatroom_members { get; set; }
+
         public DbSet<USER> Users { get; set; }
 
         public DbSet<MESSAGE> Messages { get; set; }
 
-        public DbSet<CHATROOM> Chatrooms { get; set; }
+
 
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
