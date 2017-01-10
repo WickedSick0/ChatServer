95931360f1861071d6a7f67b6b04c4c126cf2d82
WickedSick0 <nycdavid@sssvt.cz>
2017-01-10 21:40:57 +0100

    folders2

---
+++ ChatServerASP/ChatServerASP/Models/Repositories/UserRepository.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ChatServerASP.Models
+{
+    public class UserRepository
+    {
+        private MyContext _context = new MyContext();
+
+        public List<USER> FindAll()
+        {
+            return this._context.Users.ToList<USER>();
+        }
+
+        public USER FindById(int id)
+        {
+            return this._context.Users.Find(id);
+        }
+
+        public void InsertUser(USER u)
+        {
+            this._context.Users.Add(u);
+            this._context.SaveChanges();
+        }
+
+        public void UpdateUser(USER u)
+        {
+            USER utemp = this.FindById(u.ID);
+
+            utemp.Login = u.Login;
+            utemp.Nick = u.Nick;
+            utemp.Password = u.Password;
+            utemp.Photo = u.Photo;
+
+            this._context.SaveChanges();
+        }
+
+        public void DeleteUser(int id)
+        {
+            USER u = this.FindById(id);
+            this._context.Users.Remove(u);
+            this._context.SaveChanges();
+        }
+    }
+}
---
+++ ChatServerASP/ChatServerASP/Models/Tables/USER.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ChatServerASP.Models
+{
+    public class USER
+    {
+        public int ID { get; set; }
+
+        public string Login { get; set; }
+
+        public string Nick { get; set; }
+
+        public string Password { get; set; }
+
+        public string Photo { get; set; }
+    }
+}
