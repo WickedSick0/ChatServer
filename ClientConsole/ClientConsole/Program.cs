5ee3b6bf7677bc3418ecbb799abc56ef1a99a580
stipekjan <stipekjan@L114PC08>
2017-01-13 12:34:29 +0100

    fdas

--- ClientConsole/ClientConsole/Program.cs
+++ ClientConsole/ClientConsole/Program.cs
@@ -2,7 +2,9 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -31,18 +33,37 @@ namespace ClientConsole
             HttpResponseMessage response = clint.PostAsync("/api/USERs/", byteContent).Result;
         }
 
-        static void GetUser(int id)
+        static async void GetUser(int id)
         {
-            /*
-            USER user = new USER() { Nick = nick };
+
+            //USER user = new USER() { Nick = nick };
 
             HttpClient clint = new HttpClient();
             clint.BaseAddress = new Uri("http://localhost:53098/");
             HttpResponseMessage response = clint.GetAsync("/api/USERs/").Result;
-            var emp = response.Content.;
-            
+            //var emp = await response.Content.ReadAsAsync<USER>();
+
             Console.WriteLine(response.Content);
-            */
+
+        }
+        static async Task<USER> GetProductAsync(string path)
+        {
+            USER user = null;
+
+            HttpClient clint = new HttpClient();
+            clint.BaseAddress = new Uri("http://localhost:53098/");
+
+            HttpResponseMessage response = await clint.GetAsync(path);
+            if (response.IsSuccessStatusCode)
+            {
+                //user = await response.Content.ReadAsAsync<USER>();
+            }
+            return user;
+
+
+            /*
+             * https://www.asp.net/web-api/overview/advanced/calling-a-web-api-from-a-net-client
+             */
         }
     }
 }
