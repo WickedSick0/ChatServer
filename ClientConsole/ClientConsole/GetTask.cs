9150df54ffade99fed86c4e0e33dec7dcb5d1631
Stitek <stipekjan@sssvt.cz>
2017-01-13 20:13:38 +0100

    Vůbec nevíš

---
+++ ClientConsole/ClientConsole/GetTask.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace ClientConsole
+{
+    public class GetTask<T> where T : class
+    {
+        public HttpClient client = new HttpClient();
+
+        public GetTask()
+        {
+            client.BaseAddress = new Uri("http://localhost:53098/");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+        //GET
+        public async Task<T> GetUserAsync(string path)
+        {
+            T var = null;
+            HttpResponseMessage response = await this.client.GetAsync(path);
+            if (response.IsSuccessStatusCode)
+            {
+                var = await response.Content.ReadAsAsync<T>();
+            }
+            return var;
+        }
+
+        ////GET ALL
+        //async Task<T> GetUsersAsync(string path)
+        //{
+        //    T var = null;
+        //    HttpResponseMessage response = await this.client.GetAsync(path);
+        //    if (response.IsSuccessStatusCode)
+        //    {
+        //        var = await response.Content.ReadAsAsync<T>();
+        //    }
+        //    return var;
+        //}
+    }
+}
--- ClientConsole/ClientConsole/Program.cs
+++ ClientConsole/ClientConsole/Program.cs
@@ -1,69 +1,214 @@
-﻿using Newtonsoft.Json;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace ClientConsole
 {
     class Program
     {
+        static HttpClient client = new HttpClient();
+
+        private static int Mod = 0; // Urcuje Mod
+        static ConsoleKey Tlacitko = ConsoleKey.F1; // Ukladani zmacknutych tlacitek do Tlacitko
+
         static void Main(string[] args)
         {
-            //PostUser(Console.ReadLine());
-            //GetUser(Convert.ToInt32(Console.ReadLine());
+            Console.BackgroundColor = ConsoleColor.White;
 
-            Console.Read();
-        }
-
-        static void PostUser(string nick)
-        {
-            USER user = new USER() { Nick = nick };
-
-            HttpClient clint = new HttpClient();
-            clint.BaseAddress = new Uri("http://localhost:53098/");
-            var myContent = JsonConvert.SerializeObject(user);
-            var buffer = System.Text.Encoding.UTF8.GetBytes(myContent);
-            var byteContent = new ByteArrayContent(buffer);
-            byteContent.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("application/json");
-            HttpResponseMessage response = clint.PostAsync("/api/USERs/", byteContent).Result;
-        }
-
-        static async void GetUser(int id)
-        {
-
-            //USER user = new USER() { Nick = nick };
-
-            HttpClient clint = new HttpClient();
-            clint.BaseAddress = new Uri("http://localhost:53098/");
-            HttpResponseMessage response = clint.GetAsync("/api/USERs/").Result;
-            //var emp = await response.Content.ReadAsAsync<USER>();
-
-            Console.WriteLine(response.Content);
-
-        }
-        static async Task<USER> GetProductAsync(string path)
-        {
-            USER user = null;
-
-            HttpClient clint = new HttpClient();
-            clint.BaseAddress = new Uri("http://localhost:53098/");
-
-            HttpResponseMessage response = await clint.GetAsync(path);
-            if (response.IsSuccessStatusCode)
+            while (Mod != -1) // Pokud se Mod = -1 program se ukonci
             {
-                //user = await response.Content.ReadAsAsync<USER>();
-            }
-            return user;
+                if (Mod == 0) // Skoci do MenuMod 0
+                    Mod = MenuMod(); // Mod = MenuMod()
+                else if (Mod == 1) // Skoci do PlayMod 1
+                    Mod = LogInMod(); // Mod = PlayMod()
+                //else if (Mod == 2) // Skoci do OptionMod 2
+                //    Mod = OptionsMod(); // Mod = OptionsMod()
+                else if (Mod == 3) // Exit 3
+                    Mod = -1;
+            } // Konec programu   
 
+            RunAsync().Wait();
+
+            Console.ReadLine();
+        }
+
+        static int MenuMod() // MenuMod kdyz Mod = 0
+        {
+            string[] Polozky = new string[] { "                 Log in                    ", "                 Register                  ", "                 Exit                      " }; // Pole stringů položky v menu
+            int Vybrana = 0; // Urcuje vybranou polozku v menu
+
+            while (true) // Hlida tlacitka a vykresluje menu
+            {
+                VykresliMenu(Polozky, Vybrana); // Vykresluje menu
+
+                Tlacitko = Console.ReadKey().Key; // Ceka na zmacknuti tlacitka
+
+                if (Tlacitko == ConsoleKey.UpArrow) // ↑
+                {
+                    Vybrana--; // Zmensi Vybrana o 1
+                    if (Vybrana < 0) // Pokud je Vybrana mensi nez 0
+                        Vybrana = Polozky.Length - 1; // Skoci na posledni polozku (Vybrana se precisluje)
+                }
+                else if (Tlacitko == ConsoleKey.DownArrow) // ↓
+                {
+                    Vybrana++; // Zvetsi Vybrana o 1
+                    if (Vybrana > Polozky.Length - 1) // Pokud je Vybrana vetsi nez index posleni polozky
+                        Vybrana = 0; // Skoci na prvni polozku (Vybrana se precisluje)
+                }
+                else if (Tlacitko == ConsoleKey.Enter) // Pokud se zmackne Enter
+                    return Vybrana + 1; // Vrati hodnotu ktera se pouzije jako cislo modu (Vybrana = 0; - Enter -, vrati 1 jako Mod = 1)
+            }
+        } // MenuMod konec
+
+        static void VykresliMenu(string[] Polozky, int Vybrana) // Vykresluje menu
+        {
+            Console.Clear(); // Vymaze konzoli
+            Console.SetWindowSize(45, 15); // Nastavi rozmery konzole (41 + 3, 21 - 6)
+            Console.CursorVisible = false; // Kurzor neni videt
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Black;
+
+            //VykresleniNazvu(); // Vykresli Nadpis
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.BackgroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine("                THUNDER CHAT                "); // Vypise HighScore fialove
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.WriteLine();
+
+            int Krok = 0; // Pomaha vykreslit vybranou polozku cervene
+
+            foreach (string polozka in Polozky) // Vypise pole stringu
+            {
+                if (Krok == Vybrana) // Na zacatku bude prvni polozka cervena protoze Krok = 0 && Vybrana = 0
+                {
+                    Console.BackgroundColor = ConsoleColor.White; // Nastavi pozadi na cernou barvu
+                    Console.Write(" "); // Odsadí mezeru
+                    Console.ForegroundColor = ConsoleColor.White; // Nastavi vybranou polozku na cervenou barvu
+                    Console.BackgroundColor = ConsoleColor.DarkGray; // Nastavi pozadi vybrane polozky na zlutou barvu
+                    Console.WriteLine(polozka); // Vypise vsechny polozky
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Black; // Vse ostatni na zlutou barvu
+                    Console.WriteLine("  " + polozka); // Vypise vsechny polozky
+                }
+                Console.BackgroundColor = ConsoleColor.White; // Nastavi pozadi na cernou barvu
+
+                Krok++; // Zvetsi Krok o 1
+            }
+        } // VykresleniMenu konec
+
+        static int LogInMod() // PlayMod = Mod 1
+        {
+            Console.Clear();
+            RunAsync().Wait();
+            Console.ReadLine();
+            //if (LogIn.SpustitHru() == true) // Spusti se hra a pokud vrati true spusti se znova PlayMod jinak se vrati do MenuMod
+            //    return 1; // Spusti se PlayMod = Mod 1
+            //else
+            return 0; // Vrati se do MenuMod = Mod 0
+        } // PlayMod konec
+
+        static async Task RunAsync()
+        {         
+            /*
+            // Create a new user
+            USER user = new USER() { Nick = "test" };
+
+            var url = await CreateUserAsync(user);
+            Console.WriteLine($"Created at {url}");
+            */
+
+            // Get the user
+            //GetTask<USER> GetUser = new GetTask<USER>();
+            //USER user = null;
+            //user = await GetUser.GetUserAsync($"api/USERs/1");
+            //Showuser(user);
+
+            // Get users
+            GetTask<List<USER>> GetUsers = new GetTask<List<USER>>();
+            List<USER> users = await GetUsers.GetUserAsync($"api/USERs/");
+            foreach (USER item in users)
+            {
+                Console.WriteLine(item.Nick + " ");
+            }
 
             /*
-             * https://www.asp.net/web-api/overview/advanced/calling-a-web-api-from-a-net-client
-             */
+            // Update the user
+            Console.WriteLine("Updating price...");
+            user.Nick = "tttttt";
+            await UpdateuserAsync(user);
+
+            // Get the updated user
+            user = await GetUserAsync(url.PathAndQuery);
+            Showuser(user);
+
+            // Delete the user
+            var statusCode = await DeleteUserAsync(user.Id.ToString());
+            Console.WriteLine($"Deleted (HTTP Status = {(int)statusCode})");
+            */
+        }
+
+        static void Showuser(USER user)
+        {
+            Console.WriteLine($"Nick: {user.Nick}");
+        }
+
+        ////GET
+        static async Task<USER> GetUserAsync2(string path)
+        {
+            USER user = null;
+            HttpResponseMessage response = await client.GetAsync(path);
+            if (response.IsSuccessStatusCode)
+            {
+                user = await response.Content.ReadAsAsync<USER>();
+            }
+            return user;
+        }
+
+        //GET ALL
+        static async Task<List<USER>> GetUsersAsync2(string path)
+        {
+            List<USER> users = null;
+            HttpResponseMessage response = await client.GetAsync($"api/USERs/");
+            if (response.IsSuccessStatusCode)
+            {
+                users = await response.Content.ReadAsAsync<List<USER>>();
+            }
+            return users;
+        }
+
+        //POST
+        static async Task<Uri> CreateUserAsync(USER user)
+        {
+            HttpResponseMessage response = await client.PostAsJsonAsync("api/USERs", user);
+            response.EnsureSuccessStatusCode();
+
+            // return URI of the created resource.
+            return response.Headers.Location;
+        }
+
+        //PUT
+        static async Task<USER> UpdateuserAsync(USER user)
+        {
+            HttpResponseMessage response = await client.PutAsJsonAsync($"api/USERs/{user.Id}", user);
+            response.EnsureSuccessStatusCode();
+
+            // Deserialize the updated user from the response body.
+            user = await response.Content.ReadAsAsync<USER>();
+            return user;
+        }
+
+        //DELETE
+        static async Task<HttpStatusCode> DeleteUserAsync(string id)
+        {
+            HttpResponseMessage response = await client.DeleteAsync($"api/USERs/{id}");
+            return response.StatusCode;
         }
     }
 }
