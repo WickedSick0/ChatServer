048a79707e633bd29c4df79a6fb1c08bed62e7ea
okurkinos <tureklukas@sssvt.cz>
2017-02-09 13:54:34 +0100

    upload better

--- ChatServerASP/ChatServerASP/Controllers/UploadController.cs
+++ ChatServerASP/ChatServerASP/Controllers/UploadController.cs
@@ -10,6 +10,8 @@ using System.Net.Http;
 using System.Threading.Tasks;
 using System.Web;
 using System.Web.Http;
+using System.Drawing.Imaging;
+using System.Drawing;
 
 namespace ChatServerASP.Controllers
 {
@@ -18,7 +20,7 @@ namespace ChatServerASP.Controllers
         private UserRepository Urep = new UserRepository();
         private User_tokensRepository Utrep = new User_tokensRepository();
 
-        [HttpPost,Route("api/Upload/")]
+        [HttpPost, Route("api/Upload/")]
         public Task<HttpResponseMessage> PostFile()
         {
 
@@ -40,6 +42,11 @@ namespace ChatServerASP.Controllers
 
 
             HttpRequestMessage request = this.Request;
+            // obrazek max 25Mb
+            if (request.Content.Headers.ContentLength > 25000000)
+            {
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+            }
             if (!request.Content.IsMimeMultipartContent())
             {
                 throw new HttpResponseException(HttpStatusCode.UnsupportedMediaType);
@@ -48,21 +55,35 @@ namespace ChatServerASP.Controllers
             string root = System.Web.HttpContext.Current.Server.MapPath("~/Content/Photos");
             var provider = new MultipartFormDataStreamProvider(root);
 
-            string userfilepath;
+            string userfilename;
             USER u1 = Urep.FindById(id);
             var task = request.Content.ReadAsMultipartAsync(provider).
                 ContinueWith<HttpResponseMessage>(o =>
                 {
-
                     string file1 = provider.FileData.First().LocalFileName;
+                    FileInfo fileimgtest = new FileInfo(file1);
+                    try
+                    {
+
+                        Image imgtest = Image.FromFile(fileimgtest.FullName);
+                        imgtest.Dispose();
+                    }
+                    catch (Exception)
+                    {
+                        fileimgtest.Delete();
+                        throw new HttpResponseException(HttpStatusCode.UnsupportedMediaType);
+                    }
+
+
                     FileInfo fileold = new FileInfo(file1);
-                    userfilepath = fileold.Name + "-" + u1.Login + ".jpg";
-                    File.Move(fileold.FullName, Path.Combine(fileold.DirectoryName, userfilepath));
+                    userfilename = fileold.Name + "-" + u1.Login + ".jpg";
+                    File.Move(fileold.FullName, Path.Combine(fileold.DirectoryName, userfilename));
+                    string newfilepath = Path.Combine(fileold.DirectoryName, userfilename);
                     fileold.Delete();
 
-                    u1.Photo = "/Content/Photos/" + userfilepath;
+                    u1.Photo = "/Content/Photos/" + userfilename;
                     Urep.UpdateUser(u1);
-                    
+
                     return new HttpResponseMessage()
                     {
                         Content = new StringContent("File uploaded.")
@@ -70,6 +91,6 @@ namespace ChatServerASP.Controllers
                 }
             );
             return task;
-        } 
+        }
     }
 }
