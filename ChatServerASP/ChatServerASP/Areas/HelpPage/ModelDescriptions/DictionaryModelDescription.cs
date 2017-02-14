f96ae6008a4f400f0bff47b05c8aeba4edcb06e5
Stitek <stipekjan@sssvt.cz>
2017-02-14 23:29:37 +0100

    doc

---
+++ ChatServerASP/ChatServerASP/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace ChatServerASP.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ ChatServerASP/ChatServerASP/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace ChatServerASP.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
